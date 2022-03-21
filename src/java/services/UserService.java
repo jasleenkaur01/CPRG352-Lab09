57e6e91c27a5e3f267a03007295c6cf0781a0650
Samuel Afolayan <99280928+WhiteoutTK@users.noreply.github.com>
2022-03-20 21:12:27 -0600

    Update UserService.java

--- src/java/services/UserService.java
+++ src/java/services/UserService.java
@@ -14,24 +14,28 @@ public class UserService
         return users;
     }
     
+    //get new user
     public User get(String email) throws Exception {
         UserDB userDB = new UserDB();
         User user = userDB.get(email);
         return user;
     }
     
+    //insert parameters to user data
     public void insert(String email, boolean active, String fname, String lname, String password, int role) throws Exception {
         User user = new User(email, active, fname, lname, password, role);
         UserDB userDB = new UserDB();
         userDB.insert(user);
     }
     
+    //update existing user parameters
     public void update(String email, boolean active, String fname, String lname, String password, int role) throws Exception {
         User user = new User(email, active, fname, lname, password, role);
         UserDB userDB = new UserDB();
         userDB.update(user);
     }
     
+    //delete existing user parameters
     public void delete(String email) throws Exception {
         User user = new User();
         user.setEmail(email);
